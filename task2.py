433370c4c35c58c939a7e60090af1fa2a0da31b0
dedsecbuddy <81178235+dedsecbuddy@users.noreply.github.com>
2026-01-08 19:41:35 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+fname = str(input("Enter your first name:"))
+sname = str(input("Enter your second name:"))
+
+full_name = fname + " " +sname 
+
+print(f"Hello, {full_name}! Wwelcome to the python world.")
